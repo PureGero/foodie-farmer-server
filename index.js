da0ee369f6e2db19e5081abf128fbb06a0d4e105
Joshua You <joshua.you@student.uq.edu.au>
2020-08-30 11:46:29 +1000

    reorganised database code into models folder

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const expressFormData = require('express-form-data')
 
-var Sequelize = require('sequelize')
-  , sequelize = new Sequelize('FoodieFarmer', 'hejjnt', 'hejjnt', {
-      dialect: "mysql",
-      host:     "localhost",
-      port:     3306,
-    });
-
-sequelize
-  .authenticate()
-  .then(function(err) {
-    console.log('Connection has been established successfully.');
-  }, function (err) { 
-    console.log('Unable to connect to the database:', err);
-  });
-
 const PORT = 3000
 
 const app = express()
@@ -30,6 +15,9 @@ app.use(expressFormData.parse())
 const login = require('./login/login')
 const customer = require('./customer/customer')
 const farmer = require('./farmer/farmer')
+
+// foodie-farmer models
+const setup = require('./models/init')
 
 app.use(login)
 app.use('/customer', customer)
---
+++ models/init.js
@@ -0,0 +1,14 @@
+var Sequelize = require('sequelize')
+, sequelize = new Sequelize('FoodieFarmer', 'hejjnt', 'hejjnt', {
+    dialect: "mysql",
+    host:     "localhost",
+    port:     3306,
+  });
+
+sequelize
+.authenticate()
+.then(function(err) {
+  console.log('Connection has been established successfully.');
+}, function (err) { 
+  console.log('Unable to connect to the database:', err);
+});
