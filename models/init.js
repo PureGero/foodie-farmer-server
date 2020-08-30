d7ab632e97c87d106d415702013b7162654e0fe0
Joshua You <joshua.you@student.uq.edu.au>
2020-08-30 12:31:31 +1000

    removed a file and moved it into init

--- models/init.js
+++ models/init.js
@@ -13,4 +13,32 @@ sequelize
   console.log('Unable to connect to the database:', err);
 });
 
-var customerModel = require('./customerModel')
+// Model for customer
+const Customer = sequelize.define('Customer', {
+  // Model attributes are defined here
+  userName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  address: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  deliveryRoute: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  bankAccount: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  }
+}, {
+  // Other model options go here
+});
+
+// `sequelize.define` also returns the model
+console.log(Customer === sequelize.models.Customer); // true
