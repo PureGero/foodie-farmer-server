7a870f499d9b4b8d9e07f2c86aca90e004fbcb92
Joshua You <joshua.you@student.uq.edu.au>
2020-08-30 10:55:12 +1000

    Testing database connection setup on zone

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const cookieParser = require('cookie-parser')
 const express = require('express')
 const expressFormData = require('express-form-data')
 
+var Sequelize = require('sequelize')
+  , sequelize = new Sequelize('FoodieFarmer', 'root', 'hejjnt', {
+      dialect: "mysql",
+      host: "localhost",
+      port:    3306,
+    });
+
+sequelize
+  .authenticate()
+  .then(function(err) {
+    console.log('Connection has been established successfully.');
+  }, function (err) { 
+    console.log('Unable to connect to the database:', err);
+  });
+
 const PORT = 3000
 
 const app = express()
